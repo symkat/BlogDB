101b1c3e94cdad351c2e872244de13b26e461f9f
Kaitlyn Parkhurst <kparkhurst@intellisurvey.com>
2021-11-20 10:14:24 -0800

    Basic Add Blog

--- Web/lib/BlogDB/Web.pm
+++ Web/lib/BlogDB/Web.pm
@@ -87,7 +87,10 @@ sub startup ($self) {
     $auth->post( '/user/settings/email'   )->to( 'User#post_email'   )->name( 'do_user_email'    );
 
     # /blog/ routes
-    $r->post   ( '/blog/new'              )->to( 'Blog#post_new_blog'  )->name( 'do_new_blog'      ); # Create a new blog.
+    $r->get    ( '/blog/new'              )->to( 'Blog#get_new_blogs'     )->name( 'new_blogs'        ); # List new blogs.
+    $r->post   ( '/blog/new'              )->to( 'Blog#post_new_blog'     )->name( 'do_new_blog'      ); # Create a new blog.
+    $r->get    ( '/blog/new/:id'          )->to( 'Blog#get_edit_new_blog' )->name( 'edit_new_blog'    ); # Show edit a new blog page.
+    $r->post   ( '/blog/new/:id'          )->to( 'Blog#post_edit_new_blog')->name( 'do_edit_new_blog' ); # Update a new blog.
 
 #    $r->get    ( '/view/:id/:name'        )->to( 'Blog#get_blog'      )->name( 'blog'             ); # View A Specific Blog.
 #
--- Web/lib/BlogDB/Web/Controller/Blog.pm
+++ Web/lib/BlogDB/Web/Controller/Blog.pm
@@ -40,7 +40,9 @@ sub post_unpublish ($c) {
 # Handle New Blogs
 
 sub get_new_blogs ($c) {
+    $c->set_template( 'blog/new/index' );
 
+    push @{$c->stash->{blogs}}, $c->db->resultset('PendingBlog')->all;
 }
 
 sub post_new_blog ($c) {
@@ -76,10 +78,54 @@ sub post_new_blog ($c) {
 }
 
 sub get_edit_new_blog ($c) {
+    $c->set_template( 'blog/new/item' );
+
+    my $blog_id = $c->stash->{blog_id}  = $c->param('id');
+    my $blog    = $c->stash->{blog_obj} = $c->db->resultset('PendingBlog')->find( $blog_id );
+    
+
+    # Populate the form with the current values.
+
+    $c->stash->{form_title}   = $blog->title; 
+    $c->stash->{form_url}     = $blog->url;  
+    $c->stash->{form_img_url} = $blog->img_url;
+    $c->stash->{form_rss_url} = $blog->rss_url;
+    $c->stash->{form_tagline} = $blog->tagline;
+    $c->stash->{form_about}   = $blog->about;
+
+    # Make sure we have added the tags to the page.
+    push @{$c->stash->{tags}}, $c->db->resultset('Tag')->all;
 
 }
 
 sub post_edit_new_blog ($c) {
+
+    my $blog_id = $c->stash->{blog_id}  = $c->param('id');
+    my $blog    = $c->stash->{blog_obj} = $c->db->resultset('PendingBlog')->find( $blog_id );
+
+
+    # TODO: This section should be guarded by checking that the user
+    #       has a UUID that allows editing, or is a logged in user with
+    #       such permissions as allows editing it.
+    $c->stash->{form_title}   = $c->param("title"); 
+    $c->stash->{form_url}     = $c->param("url");  
+    $c->stash->{form_img_url} = $c->param("img_url");
+    $c->stash->{form_rss_url} = $c->param("rss_url");
+    $c->stash->{form_tagline} = $c->param("tagline");
+    $c->stash->{form_about}   = $c->param("about");
+
+
+    $blog->title( $c->stash->{form_title} );
+    $blog->url( $c->stash->{form_url} );
+    $blog->img_url( $c->stash->{form_img_url} );
+    $blog->rss_url( $c->stash->{form_rss_url} );
+    $blog->tagline( $c->stash->{form_tagline} );
+    $blog->about( $c->stash->{form_about} );
+
+    $blog->update;
+
+    # Send the user back to the standard GET path.
+    $c->redirect_to( $c->url_for( 'edit_new_blog', id => $blog->id ) );
 
 }
 
