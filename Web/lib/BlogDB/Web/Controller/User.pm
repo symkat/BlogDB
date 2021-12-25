b3f9a9b2497f528f026964fbb8fdca6fc0b04497
Kaitlyn Parkhurst <symkat@symkat.com>
2021-12-25 23:29:01 +0000

    User setting controllers

--- Web/lib/BlogDB/Web/Controller/User.pm
+++ Web/lib/BlogDB/Web/Controller/User.pm
@@ -24,15 +24,30 @@ sub post_unfollow ($c) {
 sub get_settings ($c) {
     $c->set_template( 'user/settings' );
 
+    my $is_adult = $c->stash->{form_adult} = $c->stash->{person}->setting('can_view_adult');
+
+
 }
 
 sub get_settings_email ($c) {
     $c->set_template( 'user/settings/email' );
 
+
 }
 
 sub get_settings_password ($c) {
     $c->set_template( 'user/settings/password' );
+
+}
+
+sub post_settings ($c) {
+    $c->set_template( 'user/settings' );
+
+    my $is_adult = $c->stash->{form_adult} = $c->param('is_adult') ? 1 : 0;
+
+    $c->stash->{person}->setting( can_view_adult => $is_adult );
+
+    $c->redirect_to( $c->url_for( 'user_settings'));
 
 }
 
@@ -48,13 +63,55 @@ sub post_about ($c) {
 }
 
 sub post_password ($c) {
-    $c->set_template( 'user/settings' );
+    $c->set_template( 'user/settings/password' );
+    
+    my $old_pass = $c->stash->{form_password}     = $c->param('password');
+    my $new_pass = $c->stash->{form_new_password} = $c->param('new_password');
+    my $confirm  = $c->stash->{form_confirm}      = $c->param('confirm');
 
+    push @{$c->stash->{errors}}, "Current password is required" unless $old_pass;
+    push @{$c->stash->{errors}}, "New password is required"     unless $new_pass;
+    push @{$c->stash->{errors}}, "Password confirm is required" unless $new_pass;
+
+    return 0 if $c->stash->{errors};
+    
+    push @{$c->stash->{errors}}, "Incorrect password"
+        unless $c->stash->{person}->auth_password->check_password($old_pass);
+
+    return 0 if $c->stash->{errors};
+
+    push @{$c->stash->{errors}}, "Password must be at least 7 chars" unless 7 > $new_pass;
+    push @{$c->stash->{errors}}, "Password and confirm password must match." unless $new_pass eq $confirm;
+    
+    return 0 if $c->stash->{errors};
+
+    $c->stash->{person}->auth_password->update_password( $new_pass );
+
+    # Throw the user out so they must login with their new password.
+    delete $c->session->{uid};
+    $c->redirect_to( $c->url_for( 'homepage' ));
 }
 
 sub post_email ($c) {
-    $c->set_template( 'user/settings' );
+    $c->set_template( 'user/settings/email' );
 
+    my $email = $c->stash->{form_email}    = $c->param('email');
+    my $pass  = $c->stash->{form_password} = $c->param('password');
+
+    push @{$c->stash->{errors}}, "Email is required." unless $email;
+    push @{$c->stash->{errors}}, "Password is required." unless $pass;
+
+    return 0 if $c->stash->{errors};
+
+    push @{$c->stash->{errors}}, "Incorrect password"
+        unless $c->stash->{person}->auth_password->check_password($pass);
+
+    return 0 if $c->stash->{errors};
+
+    $c->stash->{person}->email( $email );
+    $c->stash->{person}->update;
+    
+    $c->redirect_to( $c->url_for( 'user_settings_email'));
 }
 
 1;
