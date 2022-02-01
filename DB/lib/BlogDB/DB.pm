677f156e19ae007da6b3e73c88592a89fb8be6c2
Kaitlyn Parkhurst <symkat@symkat.com>
2022-01-31 19:04:14 -0800

    Regenerate DB to add DBIX::ResultSetNames.

--- DB/lib/BlogDB/DB.pm
+++ DB/lib/BlogDB/DB.pm
@@ -9,13 +9,13 @@ use warnings;
 
 use base 'DBIx::Class::Schema';
 
-__PACKAGE__->load_components("Schema::Config");
+__PACKAGE__->load_components("Schema::Config", "Schema::ResultSetNames");
 
 __PACKAGE__->load_namespaces;
 
 
-# Created by DBIx::Class::Schema::Loader v0.07049 @ 2021-10-06 18:19:48
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:NPmhmBYK6vnPZZUxqtXD6A
+# Created by DBIx::Class::Schema::Loader v0.07049 @ 2022-02-01 01:10:27
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:WJYkMkUHcfjCAq1mZSDZ9A
 
 our $VERSION = 1;
 
